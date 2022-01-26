e37cf0b88940256223872d7919cc3d138d75d06b
Schuman Zhang <sz@proadviser.com.au>
2022-01-26 17:41:02 +1100

    create user ok

--- app/controllers/api/v1/admins_controller.rb
+++ app/controllers/api/v1/admins_controller.rb
@@ -1,2 +1,19 @@
 class Api::V1::AdminsController < ApplicationController
+
+    def create
+        @admin = Admin.create(admin_params)
+
+        if @admin.valid?
+            render json: { admin: AdminSerializer.new(@admin) }, status: :created
+        else
+            render json: { error: "failed to create admin" }, status: :unprocessable_entity
+        end
+    end
+
+    private 
+
+    def admin_params
+        params.require(:admin).permit(:username, :password, :firstname, :lastname)
+    end
+
 end
