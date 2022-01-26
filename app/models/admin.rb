905c59542debc5bfc4758820f4fbf19f8addb709
Schuman Zhang <sz@proadviser.com.au>
2022-01-26 14:35:00 +1100

    wip

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,2 +1,5 @@
 class Admin < ApplicationRecord
+    # this automatically adds methods to set and authenticate against a BCrypt password
+    has_secure_password
+    validates :username, uniqueness: { case_sensitive: false }
 end
